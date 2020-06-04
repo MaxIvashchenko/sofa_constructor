c593b4f9f1820db76160cfbfc0a283c752403a43
Max Ivashchenko <Max@MacBook-Pro-Max.local>
2020-06-04 10:44:01 +0300

    Updates

---
+++ precache-manifest.89978224cf3d678897a1e53043b394b6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fbb774f96cb86627841ec002224bd6e",
+    "url": "/sofa_constructor/index.html"
+  },
+  {
+    "revision": "36e398814bae5d898921",
+    "url": "/sofa_constructor/static/css/main.992674a0.chunk.css"
+  },
+  {
+    "revision": "b066156ae7cb9b48782b",
+    "url": "/sofa_constructor/static/js/2.b0ee3473.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sofa_constructor/static/js/2.b0ee3473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e398814bae5d898921",
+    "url": "/sofa_constructor/static/js/main.4c941d20.chunk.js"
+  },
+  {
+    "revision": "2089741e07bc450b5e3b",
+    "url": "/sofa_constructor/static/js/runtime-main.c30abd2b.js"
+  },
+  {
+    "revision": "c9318b0793d77721fb177acaf29f3fa6",
+    "url": "/sofa_constructor/static/media/0.c9318b07.png"
+  },
+  {
+    "revision": "176e80c0693adb09f1ea3d88b9c98ef3",
+    "url": "/sofa_constructor/static/media/1.176e80c0.png"
+  },
+  {
+    "revision": "f739248b28a7a66c221de74b79bfc822",
+    "url": "/sofa_constructor/static/media/2.f739248b.png"
+  },
+  {
+    "revision": "91991c6e36ad7b7a12d53c0d1af4ad62",
+    "url": "/sofa_constructor/static/media/3.91991c6e.png"
+  }
+]);
